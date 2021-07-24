718af2f6e814f2a0de36a9a90c5f7566f1448309
DANNIEL <donghoon149@gmail.com>
2021-07-24 15:25:08 +0900

    Add: Ehcache to noteRecent
    Ehcache 설정을 완료하였습니다.
    아울러, noteRecent부분에 대해서 Ehcache를 적용했고
    잘 작동하는지도 확인하였습니다.

---
+++ src/main/java/com/rest/api/config/EhcacheConfig.java
@@ -0,0 +1,10 @@
+package com.rest.api.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class EhcacheConfig {
+}
