6e848c5b4378d19528932672690b15010438f4f8
DANNIEL <donghoon149@gmail.com>
2021-08-15 16:31:03 +0900

    Refactor: DeathnoteService
    새롭게 EC2와 RDS를 설정하였습니다.
    아울러, DeathnoteService의 코드가 너무 정리가 안되어있어서
    리팩토링을 진행하였습니다.
    추후에 더 작업이 필요해보입니다.

--- src/main/java/com/rest/api/util/NameFormatter.java
+++ src/main/java/com/rest/api/util/NameFormatter.java
@@ -1,13 +1,18 @@
 package com.rest.api.util;
 
+import com.rest.api.service.deathnote.DeathnoteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 public class NameFormatter {
+    private static Logger logger = LoggerFactory.getLogger(NameFormatter.class);
     // 이름을 공통된 format으로 활용하기 위함입니다.
-    public static String getFormattedSummonerName(String summonerName) {
-        System.out.println(URLDecoder.decode(summonerName, StandardCharsets.UTF_8).replaceAll("\\s", "").toLowerCase());
-        return URLDecoder.decode(summonerName, StandardCharsets.UTF_8).replaceAll("\\s", "").toLowerCase();
 
+    public static String getFormattedSummonerName(String summonerName) {
+        logger.info(URLDecoder.decode(summonerName, StandardCharsets.UTF_8).replaceAll("\\s", "").toLowerCase());
+        return URLDecoder.decode(summonerName, StandardCharsets.UTF_8).replaceAll("\\s", "").toLowerCase();
     }
 }
