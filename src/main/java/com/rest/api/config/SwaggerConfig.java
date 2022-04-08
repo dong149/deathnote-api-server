d2d47548b83a627a71a22484091c7c12e2981f95
Patrick Ryoo <donghoon149@gmail.com>
2022-04-08 15:46:37 +0900

    update: Swagger 설정

--- src/main/java/com/rest/api/config/SwaggerConfig.java
+++ src/main/java/com/rest/api/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.rest.api.config;
 import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,42 +11,40 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
-@Profile({"local", "prod"})
+@Configuration
 public class SwaggerConfig {
 
-    private String version;
-    private String title;
+    private static final String GROUP_NAME = "GROUP1";
+    private static final String BASE_PACKAGES = "com.rest.api.controller";
+    private static final String V1_ANT_PATH = "/api/v1/**";
+
+    private static final String TITLE = "deathnote.gg API DOCS";
+    private static final String DESCRIPTION = "this is for deathnote.gg service";
+    private static final String VERSION = "version 1.0";
+    private static final String SERVICE_URL = "https://deathnote.site";
+    private static final String EMAIL = "donghoon149@gmail.com";
 
     @Bean
     public Docket apiV1() {
-        version = "V1";
-        title = "DEATHNOTE.gg API " + version;
-
         return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .groupName(version)
+            .groupName(GROUP_NAME)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.rest.api.controller"))
-            .paths(PathSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGES))
+            .paths(PathSelectors.ant(V1_ANT_PATH))
             .build()
-            .apiInfo(apiInfo(title, version));
+            .apiInfo(apiInfo());
     }
 
-    private ApiInfo apiInfo(String title, String version) {
+    private ApiInfo apiInfo() {
         return new ApiInfo(
-            title,
-            "DEATHNOTE.gg API 서버 DOCS",
-            version,
-            "https://deathnote.gg",
-            new Contact(
-                "Contact Me",
-                "https://patrickryoo.tistory.com",
-                "donghoon149@gmail.com"),
-            "Licenses",
-            "https://patrickryoo.tistory.com",
-            new ArrayList<>()
-        );
+            TITLE,
+            DESCRIPTION,
+            VERSION,
+            SERVICE_URL,
+            new Contact("Contact Me", SERVICE_URL, EMAIL),
+            null,
+            SERVICE_URL,
+            new ArrayList<>());
     }
 }
